09093456fe40e66cb7010c3e81e0a5539725b989
Magesh Sridhar <mags2smart@gmail.com>
2024-12-29 15:44:27 -0700

    Initial Commit

---
+++ Made For You Card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Made For You Card
+//
+//  Created by Magesh Sridhar on 12/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Made For You Card/Made_For_You_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Made_For_You_CardApp.swift
+//  Made For You Card
+//
+//  Created by Magesh Sridhar on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Made_For_You_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
